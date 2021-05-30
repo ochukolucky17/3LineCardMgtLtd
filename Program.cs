7e1189d3a14ea06e733381cddc7857ee59cffb5b
Ochuko Lucky <ochukolucky2017@yahoo.com>
2021-05-30 23:50:53 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+
+namespace _3LineCardMgtLtd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] N = new int[6] { 1, 5, 7, 4, 1, 2};
+           
+            Solution s = new Solution();
+            Console.WriteLine(s.Thesmallest(N));
+        }
+    }
+    class Solution
+    {
+        public int Thesmallest(int[] A) {
+            if (!A.Contains(1))
+            {
+                return 1;
+            }
+            int result = A.Max();
+            foreach (var num in A)
+            {
+                if (num > 0 && num <=  result)
+                {
+                    if (!A.Contains(num +1))
+                    {
+                        result = (num + 1);
+                    }
+                }
+            }
+            return result;
+
+        }
+      
+    }
+}
